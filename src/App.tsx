48c0f76baabac795f65a3733084636e05b11d467
hitesh941 <125550519+hitesh941@users.noreply.github.com>
2026-09-10 11:48:05 +0530

    Add digital marketing packages route

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Blog from "./pages/Blog";
 import Top8AgenciesAhmedabad from "./pages/blog/Top8AgenciesAhmedabad";
+import DigitalMarketingPackagesAhmedabad from "./pages/blog/DigitalMarketingPackagesAhmedabad";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -22,6 +23,10 @@ const App = () => (
           <Route
             path="/blog/top-8-digital-marketing-agencies-in-ahmedabad"
             element={<Top8AgenciesAhmedabad />}
+          />
+          <Route
+            path="/blog/digital-marketing-packages-in-ahmedabad"
+            element={<DigitalMarketingPackagesAhmedabad />}
           />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
