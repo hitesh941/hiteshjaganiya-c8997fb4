42b86cc2fdec43f70676e69186b69d334d3a25ea
hitesh941 <125550519+hitesh941@users.noreply.github.com>
2026-09-11 15:57:20 +0530

    Add latest insights before featured media

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,6 +3,7 @@ import HeroSection from "@/components/HeroSection";
 import AboutSection from "@/components/AboutSection";
 import ServicesSection from "@/components/ServicesSection";
 import ResultsSection from "@/components/ResultsSection";
+import LatestInsightsSection from "@/components/LatestInsightsSection";
 import ContactSection from "@/components/ContactSection";
 import Footer from "@/components/Footer";
 
@@ -15,6 +16,7 @@ const Index = () => {
         <AboutSection />
         <ServicesSection />
         <ResultsSection />
+        <LatestInsightsSection />
         <ContactSection />
       </main>
       <Footer />
